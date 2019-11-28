8e23fe0b2ea7295adfa6cccf3e2a40a3fb5d3ae3
selflegal <selflegal@kaminohajimenoMacBook-puro.local>
2019-11-29 06:20:20 +0900

    add jsc

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function App() {
-  return <h1>ちょんちょん、ちょーんちょん</h1>
+  return (
+    <React.Fragment>
+      <label htmlFor="bar">bar</label>
+      <input type="text" onChange={() => {console.log("clicked")}}></input>
+    </React.Fragment>
+  )
 }
 
 
