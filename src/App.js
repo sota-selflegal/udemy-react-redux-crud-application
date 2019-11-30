d2ecb7c984ce1a1b3a380a50b736cc3f0ff69deb
selflegal <selflegal@kaminohajimenoMacBook-puro.local>
2019-11-30 17:32:31 +0900

    leatn prop-types

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
 const App = () => {
   const profiles = [
-    {name: "New-New", age: 5},
-    {name: "Kama-Kama", age: 8},
-    {name: "En-En", age: 5},
-    {name: "Chon-Chon", age: 5},
-    {}
+    {name: "New-New", age: 25},
+    {name: "Kama-Kama", age: 28},
+    {name: "En-En", age: 23},
+    {name: "Chon-Chon", age: 23},
+    {name: "ss" }
   ]
   return (
     <div>
@@ -23,9 +24,8 @@ const User = (props) => {
   return <div>Hi, I am {props.name}, and {props.age} years old</div>
 }
 
-User.defaultProps = {
-  age: "不定",
-  name: "不定"
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired
 }
-
 export default App;
