97870833186638a76b48713388286aa9f3497713
selflegal <selflegal@kaminohajimenoMacBook-puro.local>
2019-11-30 11:24:39 +0900

    add componets

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
 
-function App() {
-  return (
-    <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onChange={() => {console.log("clicked")}}></input>
-    </React.Fragment>
-  )
+//function App() {
+//  return (
+//    <React.Fragment>
+//      <label htmlFor="bar">bar</label>
+//      <input type="text" onChange={() => {console.log("clicked")}}></input>
+//    </React.Fragment>
+//  )
+//}
+
+const Cat = () => {
+  return <div>Hi!</div>
 }
 
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
+}
 
 export default App;
